000cdd7a8f7f28f8aa2edd731d3ab28b08e8bac4
James Hennessey <jameswhennessey@gmail.com>
2014-07-11 14:18:20 +0100

    Started working on custom slicer

--- pyramid_gcode.cpp
+++ pyramid_gcode.cpp
@@ -12,6 +12,7 @@ float current_width = 0.0;
 const float layer_height = 0.1;
 const float nozzle_width = 0.4;
 const int inner_wall_width = 3;
+const float e_coef = 0.0329;
 
 
 void addCommand(std::vector<std::string> *gcode, float x, float y, float e = 0.0, float z = 0.0)
@@ -69,15 +70,18 @@ void addPyramidLayer(std::vector<std::string> *gcode)
 {
   std::cout << "New Layer \n";
   float w = current_width;
+  float e = 0.0;
   for(int i = 1; i <= inner_wall_width; i++)
   {
-    addCommand(gcode, w, 0, 0.1);
-    addCommand(gcode, 0.0, w, 0.1);
-    addCommand(gcode, -w, 0, 0.1);
-    addCommand(gcode, 0.0, -w, 0.1);
+    e = w * e_coef;
+    addCommand(gcode, w, 0, e);
+    addCommand(gcode, 0.0, w, e);
+    addCommand(gcode, -w, 0, e);
+    addCommand(gcode, 0.0, -w, e);
     addCommand(gcode, nozzle_width, nozzle_width);
     w = w - (nozzle_width * 2);
   }
+  addCommand(gcode, -(nozzle_width * inner_wall_width), -(nozzle_width * inner_wall_width));
 }
 
 void createPyramid(const char* argv[])
@@ -85,11 +89,11 @@ void createPyramid(const char* argv[])
     std::cout << "Pyramid Starting Width " << argv[1] << " \n"; 
     current_width = atoi(argv[1]);
     std::vector<std::string> gcode;
-    addCommand(&gcode, 100.0, 100.0, 0.0, 0.30);
+    addCommand(&gcode, 50.0, 50.0, 0.0, 0.30);
     while(current_width > (nozzle_width * inner_wall_width * 4))
     {
       addPyramidLayer(&gcode);
-      current_width = current_width - (nozzle_width * inner_wall_width * 2);
+      current_width = current_width - (nozzle_width * 2);
       addCommand(&gcode, nozzle_width, nozzle_width, 0.0, 0.10);
     }
     writeToFile(&gcode);
